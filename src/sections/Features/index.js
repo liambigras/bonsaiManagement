081a4219fb2dc55486b55d73f9b00897711d7315
liambigras <liambigras@hotmail.com>
2022-09-18 19:40:09 -0400

    fixed image

--- src/sections/Features/index.js
+++ src/sections/Features/index.js
@@ -1,10 +1,11 @@
-
+import useLocales from '../../hooks/useLocales';
 
 const Features = () => {
+    const { translate } = useLocales();
     return (
-        <div className='flex justify-center '>
+        <div className='flex justify-center pt-20'>
             <div className="max-w-7xl w-full py-20 mx-4 flex">
-                <h1 className="font-bold text-5xl flex  w-full justify-center">Features</h1>
+                <h1 className="font-bold text-5xl flex  w-full justify-center">{translate("navbar.features")}</h1>
             </div>
         </div>
         
--- src/sections/Home/index.js
+++ src/sections/Home/index.js
@@ -12,7 +12,7 @@ const Block = (props) => {
 	const position = inViewport ? 100 : 0;
    
     return (
-        <div className='flex justify-center pt-20' ref={forwardedRef}>
+        <div className='flex justify-center pt-48' ref={forwardedRef}>
         <div className="max-w-7xl w-full mx-4 grid sm:grid-cols-1 md:grid-cols-3  duration-700" style={{ opacity: opacity, transform: `translateX(${size && size.width > 800 ? position : 0}px)` }}>
             <h1 className='text-4xl lg:text-6xl font-bold leading-tight md:col-span-2' >{translate("homepage.title")}</h1>
             <span />
@@ -30,7 +30,7 @@ const ViewportBlock = handleViewport(Block);
 const Home = () => {
     
     return (
-    <div className='w-full h-screen bg-auto font-poppins bg-center' style={{backgroundImage:"url(/bonsaiManagement/ressources/bgbanner.jpg)"}}>
+    <div className='relative w-full h-[100vh] bg-center bg-cover font-poppins ' style={{backgroundImage:"url(/bonsaiManagement/ressources/bgbanner.jpg)"}}>
         
         <Navbar />
         <ViewportBlock  />
